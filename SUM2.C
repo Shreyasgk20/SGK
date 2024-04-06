1009a6e8756ca8cbf6d042bcadd6b078b677dcc1
BCA <bca@SURANAUG.com>
2024-04-06 14:50:06 +0530

    implemented program

---
+++ SUM2.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c;
+	clrscr();
+	printf("\nEnter 2 numbers:");
+	scanf("%d%d",&a,&b);
+	c=a+b;
+	printf("\nSum of 2 numbers is %d",c);
+	getch();
+}
